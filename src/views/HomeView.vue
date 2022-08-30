463e3ba585b732158ebe4ee28d4a66ddb041e6db
Bahador Aryaeefar <bahadoraryaeefar@gmail.com>
2022-08-30 16:19:34 +0430

    search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="home">
-    <img class="mx-auto" alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="relative w-4/5 h-[4rem] mx-auto mt-[7.6rem] z-10 text-lg font-medium text-job-darkCyan">
+    <span
+      class="absolute top-[calc(50%-0.875rem)] right-8 border-b-[2px] border-transparent hover:border-job-darkCyan cursor-pointer">clear</span>
+    <input class="w-full h-full bg-white rounded-[0.25rem] focus:outline-none blueShadow pl-8" type="text">
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'job': {
+          darkCyan: "hsl(180,29%,50%)",
+          lightGrayishCyan: "hsl(180,52%,96%)",
+          darkGrayishCyan: "hsl(180,8%,52%)",
+          veryDarkGrayishCyan: "hsl(180,14%,20%)"
+        },
+      }
+    },
   },
   plugins: [],
 }
