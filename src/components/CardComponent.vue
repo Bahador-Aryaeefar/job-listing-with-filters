da4b880fa5613de2a52ca8b99b8ffc6f0d89d02a
Bahador Aryaeefar <bahadoraryaeefar@gmail.com>
2022-08-31 00:58:48 +0430

    first card

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div
+        class="relative overflow-hidden w-4/5 bg-white mt-10 mx-auto blueShadow rounded-[0.25rem] px-10 py-8 flex justify-between after:h-full after:w-1.5 after:bg-job-darkCyan after:absolute after:left-0 after:top-0 after:scale-y-0 hover:after:scale-y-100 after:transition-all">
+        <div class="flex items-center gap-6 text-lg font-medium text-job-darkCyan">
+            <img src="../assets/photosnap.svg" alt="">
+            <div class="space-y-2">
+                <div class="flex items-center">
+                    photosnap
+                    <span class="text-white bg-job-darkCyan rounded-full pt-1 px-2 text-sm ml-4">NEW!</span>
+                    <span class="text-white bg-job-gray rounded-full pt-1 px-2 text-sm ml-2">FEATURED</span>
+                </div>
+                <div class="text-2xl font-bold">Senior Frontend Developer</div>
+                <div class="text-[#868e8e] flex gap-4 items-center">
+                    1d ago
+                    <span class="w-1 h-1 rounded-full bg-[#c9c9c9] mb-1"></span>
+                    Full Time
+                    <span class="w-1 h-1 rounded-full bg-[#c9c9c9] mb-1"></span>
+                    USA only
+                </div>
+            </div>
+        </div>
+
+        <div class="flex items-center gap-4 text-lg font-medium text-job-darkCyan">
+            <span
+                class="h-[2rem] bg-job-lightGrayishCyan px-2 pt-0.5 rounded-[0.25rem] hover:text-white hover:bg-job-darkCyan cursor-pointer">front</span>
+            <span
+                class="h-[2rem] bg-job-lightGrayishCyan px-2 pt-0.5 rounded-[0.25rem] hover:text-white hover:bg-job-darkCyan cursor-pointer">front</span>
+
+            <span
+                class="h-[2rem] bg-job-lightGrayishCyan px-2 pt-0.5 rounded-[0.25rem] hover:text-white hover:bg-job-darkCyan cursor-pointer">front</span>
+
+        </div>
+    </div>
+
+</template>
--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { computed } from '@vue/reactivity';
 import { ref } from 'vue';
+import CardComponent from './CardComponent.vue';
 
 const input = ref("")
 const isOpen = ref(false)
@@ -17,36 +18,41 @@ function inputBlur(event)
 </script>
 
 <template>
-    <div
-        class="relative w-4/5 mx-auto mt-[7.6rem] z-10 text-lg font-medium text-job-darkCyan bg-white blueShadow rounded-[0.25rem]">
-        <span @click="picked.clear(); input = ''"
-            class="absolute top-[1.125rem] right-8 border-b-[2px] border-transparent hover:border-job-darkCyan cursor-pointer">clear</span>
-        <input class="w-full bg-transparent h-[4rem] focus:outline-none pl-8" type="text" v-model="input"
-            @focus="isOpen = true" @blur="inputBlur">
-        <hr class="mx-8">
-        <ul class="flex min-h-[4rem] gap-6 px-8 py-4 flex-wrap">
-            <li v-for="(item, index) in picked" :key="index"
-                class="flex items-center h-[2rem] pl-2 bg-red-100 bg-job-lightGrayishCyan rounded-[0.25rem] overflow-hidden">
-                {{  item  }}
-                <div @click="picked.delete(item)"
-                    class="h-full w-[2rem] bg-job-darkCyan ml-2 hover:bg-[#2d3937] cursor-pointer flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
-                        <path fill="#FFF" fill-rule="evenodd"
-                            d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z" />
-                    </svg>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div id="drop" class="relative w-4/5 mx-auto mt-4 z-10 bg-white rounded-[0.25rem] p-2" v-if="isOpen" tabindex="0">
-        <div class="w-full h-[20rem] overflow-scroll FirefoxScroll rtl">
-            <ul class="text-lg w-full font-medium text-job-darkCyan p-2 ltr">
-                <li v-for="(item, index) in showCategories" :key="index"
-                    @click.prevent="picked.add(item); isOpen = false; input = ''"
-                    class="px-8 py-2 hover:bg-job-lightGrayishCyan rounded-[0.25rem] cursor-pointer">{{  item  }}</li>
+    <div class="z-10 relative">
+        <div
+            class="relative w-4/5 mx-auto mt-[7.6rem]  text-lg font-medium text-job-darkCyan bg-white blueShadow rounded-[0.25rem]">
+            <span @click="picked.clear(); input = ''"
+                class="absolute top-[1.125rem] right-8 border-b-[2px] border-transparent hover:border-job-darkCyan cursor-pointer">clear</span>
+            <input class="w-full bg-transparent h-[4rem] focus:outline-none pl-8" type="text" v-model="input"
+                @focus="isOpen = true" @blur="inputBlur">
+            <hr class="mx-8">
+            <ul class="flex min-h-[4rem] gap-6 px-8 py-4 flex-wrap">
+                <li v-for="(item, index) in picked" :key="index"
+                    class="flex items-center h-[2rem] pl-2 bg-job-lightGrayishCyan rounded-[0.25rem] overflow-hidden">
+                    {{  item  }}
+                    <div @click="picked.delete(item)"
+                        class="h-full w-[2rem] bg-job-darkCyan ml-2 hover:bg-[#2d3937] cursor-pointer flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+                            <path fill="#FFF" fill-rule="evenodd"
+                                d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z" />
+                        </svg>
+                    </div>
+                </li>
             </ul>
         </div>
+        <div id="drop"
+            class="absolute -bottom-[22rem] left-0 right-0 w-4/5 mx-auto mt-4 z-10 bg-white rounded-[0.25rem] p-2"
+            v-if="isOpen" tabindex="0">
+            <div class="w-full h-[20rem] overflow-scroll FirefoxScroll rtl">
+                <ul class="text-lg w-full font-medium text-job-darkCyan p-2 ltr">
+                    <li v-for="(item, index) in showCategories" :key="index"
+                        @click.prevent="picked.add(item); isOpen = false; input = ''"
+                        class="px-8 py-2 hover:bg-job-lightGrayishCyan rounded-[0.25rem] cursor-pointer">{{  item  }}</li>
+                </ul>
+            </div>
+        </div>
     </div>
+    <CardComponent></CardComponent>
 </template>
 
 <style scoped>
